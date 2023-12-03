c20975e1478c7cb54f792f1c65096f8cacc47ff6
meeramenon07 <46912602+meeramenon07@users.noreply.github.com>
2023-12-03 07:18:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+#print(f"The first grocery item to buy is {grocery_list[0]}.")
+import random
+greetings = ["hello","namaste","namashkar","vanakkam","bonjour"]
+index = random.randint(0,4) 
+print(greetings[index])
+
+
+
+
+
+
+
+
+
